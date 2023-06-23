868eb71b6f9c004d6188ba7b2004f5f5314c900a
Jun Hyeok, Kim <fixme1537@gmail.com>
2023-06-23 13:50:44 +0900

    [1] Init Crypto Project and Color set up

--- CryptoTracker/ContentView.swift
+++ CryptoTracker/ContentView.swift
@@ -10,15 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("Hello world!")
+                .foregroundStyle(Color.theme.accent)
+            Text("Hello world!")
+                .foregroundStyle(Color.theme.red)
+            Text("Hello world!")
+                .foregroundStyle(Color.theme.green)
+            Text("Hello world!")
+                .foregroundStyle(Color.theme.secondaryText)
+            Text("Hello world!")
+                .foregroundStyle(Color.theme.accent)
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
---
+++ CryptoTracker/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  CryptoTracker
+//
+//  Created by Jun Hyeok Kim on 6/23/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
