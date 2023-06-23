3f2119ddbd3697a4777a15af8a27fbf61cccfd7d
Jun Hyeok, Kim <fixme1537@gmail.com>
2023-06-23 15:53:42 +0900

    [3] Coin model setting

---
+++ CryptoTracker/Model/Coin.swift
@@ -0,0 +1,75 @@
+//
+//  Coin.swift
+//  CryptoTracker
+//
+//  Created by Jun Hyeok Kim on 6/23/23.
+//
+
+import Foundation
+
+// URL
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&locale=en
+
+
+struct Coin: Codable {
+    let id, symbol, name: String?
+    let image: String?
+    let marketCap, marketCapRank, fullyDilutedValuation: Int?
+    let currentPrice: Double
+    let totalVolume, high24H, low24H: Int?
+    let priceChange24H, priceChangePercentage24H, marketCapChange24H, marketCapChangePercentage24H: Double?
+    let circulatingSupply, totalSupply, maxSupply, ath: Int?
+    let athChangePercentage: Double?
+    let athDate: String?
+    let atl, atlChangePercentage: Double?
+    let atlDate: String?
+    let lastUpdated: String?
+    let sparklineIn7D: SparklineIn7D?
+    let currentHoldings : Double?
+
+    enum CodingKeys: String, CodingKey {
+        case id, symbol, name, image
+        case currentPrice = "current_price"
+        case marketCap = "market_cap"
+        case marketCapRank = "market_cap_rank"
+        case fullyDilutedValuation = "fully_diluted_valuation"
+        case totalVolume = "total_volume"
+        case high24H = "high_24h"
+        case low24H = "low_24h"
+        case priceChange24H = "price_change_24h"
+        case priceChangePercentage24H = "price_change_percentage_24h"
+        case marketCapChange24H = "market_cap_change_24h"
+        case marketCapChangePercentage24H = "market_cap_change_percentage_24h"
+        case circulatingSupply = "circulating_supply"
+        case totalSupply = "total_supply"
+        case maxSupply = "max_supply"
+        case ath
+        case athChangePercentage = "ath_change_percentage"
+        case athDate = "ath_date"
+        case atl
+        case atlChangePercentage = "atl_change_percentage"
+        case atlDate = "atl_date"
+        case lastUpdated = "last_updated"
+        case sparklineIn7D = "sparkline_in_7d"
+        case currentHoldings
+    }
+    
+    func updateHoldings(amount: Double) -> Coin {
+        return Coin(id: id, symbol: symbol, name: name, image: image, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, currentPrice: currentPrice, totalVolume: totalVolume, high24H: high24H, low24H: low24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated, sparklineIn7D: sparklineIn7D, currentHoldings: amount)
+    }
+    
+    
+    var currentHoldingsValue: Double {
+        return (currentHoldings ?? 0) * currentPrice
+    }
+    
+    var rank: Int {
+        return Int(marketCap ?? 0)
+    }
+    
+}
+
+// MARK: - SparklineIn7D
+struct SparklineIn7D: Codable {
+    let price: [Double]
+}
