869ba7650a301ce3df07d50e94295a989648f69f
Jun Hyeok, Kim <fixme1537@gmail.com>
2023-06-26 21:54:58 +0900

    [11] Searchbar Filtering funtionality

--- CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -28,11 +28,36 @@ class HomeViewModel : ObservableObject {
     
     // MARK: - Add Subscription
     private func addSubscription() {
-        coinDataService.$allcoins
-            .sink { [weak self] coin in
-                self?.allCoins = coin
+//        coinDataService.$allcoins
+//            .sink { [weak self] coin in
+//                self?.allCoins = coin
+//            }
+//            .store(in: &cancellables)
+        
+        $searchText
+            .combineLatest($allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoin)
+            .sink { [weak self] returnedCoin in
+                self?.allCoins = returnedCoin
             }
             .store(in: &cancellables)
+        
+        
+    }
+    
+    
+    private func filterCoin(text: String, allCoins: [Coin]) -> [Coin] {
+        guard !text.isEmpty else { return  allCoins }
+        
+        let lowercasedText = text.lowercased() // Filtering is kinda case sensitive
+        
+        let filteredCoins = allCoins.filter { coin in
+            return coin.name.lowercased().contains(lowercasedText) ||
+                 coin.symbol.lowercased().contains(lowercasedText) ||
+                 coin.id.lowercased().contains(lowercasedText)
+        }
+        return filteredCoins
     }
     
 }
