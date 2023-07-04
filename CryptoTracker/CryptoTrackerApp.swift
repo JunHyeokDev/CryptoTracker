06f0bbfa67314e994099ed9d58e41195f214b257
Jun Hyeok, Kim <fixme1537@gmail.com>
2023-07-04 22:19:53 +0900

    [23] Create Loading view and its animation.

--- CryptoTracker/CryptoTrackerApp.swift
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -16,6 +16,7 @@ struct CryptoTrackerApp: App {
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
         UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        
     }
     
     var body: some Scene {
